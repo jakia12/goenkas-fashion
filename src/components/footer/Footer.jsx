b10cfaea6b28227d0411ef0cf8708f21815e2c65
jakia12 <owasi110@gmail.com>
2023-04-15 13:45:37 +0600

    footer responsive done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,9 @@ import { AiFillInstagram } from 'react-icons/ai';
 const Footer = () => {
     return (
         <>
-            <footer className="footer sm:grid-cols- md:grid-cols-4 p-20 bg-gray-200 text-neutral-content ">
+            <footer className="footer grid-cols-1 sm:grid-cols-2 md:grid-cols-4 p-20 bg-gray-200 text-neutral-content ">
                 {/* social icon */}
-                <div>
+                <div className="text-center md:text-left">
                     <span className="footer-title text-base font-semibold text-[#191919] uppsercase">Find Us on</span>
                     <div className='flex items-center gap-3  justify-center'>
                         <div className="w-7 h-7 rounded-full bg-[#3b5998] hover:bg-[#3b5898e7] text-white hover:text-white flex items-center justify-center">
@@ -33,27 +33,27 @@ const Footer = () => {
                     </div>
 
                 </div>
-                <div>
+                <div className="text-center md:text-left">
                     <span className="footer-title text-base font-semibold text-[#191919]">ABERDEEN BAZAR</span>
                     <Link href=" " className="link link-hover text-gray-800">Mb 80, Rk Complex 1st Floor,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Berdeen Bazar, Port Blair,</Link>
                     <Link href=" " className="link link-hover text-gray-800">South Andaman,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Andaman And Nicobar Island</Link>
                 </div>
-                <div>
+                <div className="text-center md:text-left">
                     <span className="footer-title text-base font-semibold text-[#191919]">BHATU BASTI</span>
                     <Link href=" " className="link link-hover text-gray-800">Ground Floor,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Near Axis Bank, Bhatu Basti,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Port Blair, South Andaman,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Andaman And Nicobar Island,</Link>
                 </div>
-                <div>
+                <div className="text-center md:text-left">
                     <span className="footer-title text-base font-semibold text-[#191919]">BAMBOOFLAT JETTY </span>
                     <Link href=" " className="link link-hover text-gray-800">Near Bambooflat Jetty,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Bambooflat,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Port Blair, South Andaman,</Link>
                 </div>
-                <div>
+                <div className="text-center md:text-left">
                     <span className="footer-title text-base font-semibold text-[#191919]">DURGA MARKET</span>
                     <Link href=" " className="link link-hover text-gray-800">1st Floor, New Durga Market,</Link>
                     <Link href=" " className="link link-hover text-gray-800">Madhupur Road, Diglipur,</Link>
